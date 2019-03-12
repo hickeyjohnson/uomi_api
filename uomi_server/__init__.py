2f0594e9d270d4301fec750d97ba8ddacd247682
Matt Hickey <mjh060@mun.ca>
2019-03-12 19:09:10 -0230

    foo

--- uomi_server/__init__.py
+++ uomi_server/__init__.py
@@ -9,4 +9,4 @@ logging.basicConfig(level=logging.INFO)
 app = connexion.App(__name__, specification_dir='./swagger/')
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('swagger.yaml', arguments={'title': 'UOMI API'})
-app.run()
+# app.run()
