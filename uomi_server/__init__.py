4e702c00aef9a1e500329aa8e65215fb1fbd1ecb
Matt Hickey <mjh060@mun.ca>
2019-03-12 19:01:05 -0230

    try again

--- uomi_server/__init__.py
+++ uomi_server/__init__.py
@@ -1,3 +1,12 @@
 from uomi_server.__main__ import main
 
+import connexion
+import logging
+from uomi_server import encoder
+
 # main()
+logging.basicConfig(level=logging.INFO)
+app = connexion.App(__name__, specification_dir='./swagger/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('swagger.yaml', arguments={'title': 'UOMI API'})
+app.run()
--- uomi_server/__main__.py
+++ uomi_server/__main__.py
@@ -4,16 +4,16 @@ import connexion
 import logging
 from uomi_server import encoder
 
-def main():
-    """
-    main function
-    """
-    logging.basicConfig(level=logging.INFO)
-    app = connexion.App(__name__, specification_dir='./swagger/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'UOMI API'})
-    app.run()
-
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     """
+#     main function
+#     """
+#     logging.basicConfig(level=logging.INFO)
+#     app = connexion.App(__name__, specification_dir='./swagger/')
+#     app.app.json_encoder = encoder.JSONEncoder
+#     app.add_api('swagger.yaml', arguments={'title': 'UOMI API'})
+#     app.run()
+#
+#
+# if __name__ == '__main__':
+#     main()
