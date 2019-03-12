59b5aeea9832ce6a6f84174bd0d68e14976b42de
Matt Hickey <mjh060@mun.ca>
2019-03-12 19:34:36 -0230

    testing new database

--- uomi_server/database_util/__init__.py
+++ uomi_server/database_util/__init__.py
@@ -1,9 +1,12 @@
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import create_engine
+import os
+
+DATABASE_URL = os.environ['DATABASE_URL']
 
 Session = sessionmaker()
 uri = "postgresql://matthewhickey@localhost/uomi_db"
-engine = create_engine(uri)
+engine = create_engine(DATABASE_URL)
 
 Session.configure(bind=engine)
 
