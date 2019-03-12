73efb4a0dec35646bc07b40f7a38a8a0ce276d10
Matt Hickey <mjh060@mun.ca>
2019-03-12 18:32:11 -0230

    not specifying post

--- uomi_server/__main__.py
+++ uomi_server/__main__.py
@@ -12,7 +12,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'UOMI API'})
-    app.run(port=8080)
+    app.run()
 
 
 if __name__ == '__main__':
