7bfe6f33a3ec0facdc445cb83eaedc85782c06a8
Matt Hickey <mjh060@mun.ca>
2019-03-16 19:18:28 -0230

    making it possible to test local changes

--- uomi_server/database_util/__init__.py
+++ uomi_server/database_util/__init__.py
@@ -2,10 +2,14 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import create_engine
 import os
 
-DATABASE_URL = os.environ['DATABASE_URL']
+
+try:
+    DATABASE_URL = os.environ['DATABASE_URL']
+except:
+    print("No DATABASE_URL detected on os.environ... Assuming localhost")
+    DATABASE_URL = "postgresql://matthewhickey@localhost/uomi_db"
 
 Session = sessionmaker()
-uri = "postgresql://matthewhickey@localhost/uomi_db"
 engine = create_engine(DATABASE_URL)
 
 Session.configure(bind=engine)
